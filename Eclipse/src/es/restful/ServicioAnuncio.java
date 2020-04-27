1b8373f0c3f23dbb31e5cd7c0314210305d3f6dd
Alfonso Escobar <alfonso.escobar.torrico@gmail.com>
2020-04-27 19:55:58 +0200

    Terminando la clase ServicioAnuncio

--- Eclipse/src/es/restful/ServicioAnuncio.java
+++ Eclipse/src/es/restful/ServicioAnuncio.java
@@ -2,7 +2,9 @@ package es.restful;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -22,6 +24,7 @@ import javax.ws.rs.core.UriInfo;
 
 import es.dao.DAOException;
 import es.dao.mysql.MySQLAnuncioDAO;
+import es.dao.util.InfoAnuncio;
 import es.modelos.Anuncio;
 
 @ApplicationScoped
@@ -33,7 +36,6 @@ public class ServicioAnuncio {
 	
 	MySQLAnuncioDAO claseAnuncio;
 	
-	
 	@GET
 	@Path("/{tipo}/{localidad}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -42,11 +44,11 @@ public class ServicioAnuncio {
 		
 		claseAnuncio = new MySQLAnuncioDAO(dataSource);
 		Response.Status respuesta = Response.Status.OK;
-		List<Anuncio> listaAnuncio = null;
+		LinkedList<InfoAnuncio> listaAnuncio = null;
 		
 		try {
 			
-			listaAnuncio = claseAnuncio.obtenerPorParametro(localidad, tipo);
+			listaAnuncio = claseAnuncio.listaInfoAnuncios(localidad, tipo);
 			
 		} catch (DAOException e) {
 			respuesta = Response.Status.INTERNAL_SERVER_ERROR;
@@ -62,21 +64,98 @@ public class ServicioAnuncio {
 			return Response.ok(listaAnuncio).build();
 		else
 			return Response.status(respuesta).build();
-		
 	}
 	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAnuncioT(){
+	public Response getAnuncioT(@Context UriInfo uriInfo){
 		
 		claseAnuncio = new MySQLAnuncioDAO(dataSource);
+		CriterioBusqueda criterio = new CriterioBusqueda();
+		
 		Response.Status respuesta = Response.Status.OK;
-		List<Anuncio> listaAnuncio = null;
+		List<InfoAnuncio> listaAnuncio = null;
+		
+		for(Map.Entry entry : uriInfo.getQueryParameters().entrySet()) {
+			
+			switch(entry.getKey().toString()) {
+				case "localidad":
+					criterio.setLocalidad(entry.getValue().toString());
+					break;
+				case "calle":
+					criterio.setCalle(entry.getValue().toString());
+					break;
+				case "piso":
+					criterio.setPiso(Integer.parseInt(entry.getValue().toString()));
+					break;
+				case "min_metros2":
+					criterio.setMin_metros2(Double.parseDouble(entry.getValue().toString()));
+					break;
+				case "max_metros2":
+					criterio.setMax_metros2(Double.parseDouble(entry.getValue().toString()));
+					break;
+				case "num_habitaciones":
+					criterio.setNum_habitaciones(Integer.parseInt(entry.getValue().toString()));
+					break;
+				case "max_num_habitaciones":
+					criterio.setMax_num_habitaciones(Integer.parseInt(entry.getValue().toString()));
+					break;
+				case "num_banos":
+					criterio.setNum_banos(Integer.parseInt(entry.getValue().toString()));
+					break;
+				case "max_num_banos":
+					criterio.setMax_num_banos(Integer.parseInt(entry.getValue().toString()));
+					break;
+				case "tipo_edificacion":
+					criterio.setTipo_anuncio(entry.getValue().toString());
+					break;
+				case "tipo_obra":
+					criterio.setTipo_obra(entry.getValue().toString());
+					break;
+				case "equipamiento":
+					criterio.setEquipamiento(entry.getValue().toString());
+					break;
+				case "exteriores":
+					criterio.setExteriores(entry.getValue().toString());
+					break;
+				case "garaje":
+					criterio.setGaraje(Boolean.parseBoolean(entry.getValue().toString()));
+					break;
+				case "trastero":
+					criterio.setTrastero(Boolean.parseBoolean(entry.getValue().toString()));
+					break;
+				case "ascensor":
+					criterio.setAscensor(Boolean.parseBoolean(entry.getValue().toString()));
+					break;
+				case "ultima_planta":
+					criterio.setUltima_planta(Boolean.parseBoolean(entry.getValue().toString()));
+					break;
+				case "mascotas":
+					criterio.setMascotas(Boolean.parseBoolean(entry.getValue().toString()));
+					break;
+				case "tipo_anuncio":
+					criterio.setTipo_anuncio(entry.getValue().toString());
+					break;
+				case "min_precio":
+					criterio.setMin_precio(Double.parseDouble(entry.getValue().toString()));
+					break;
+				case "max_precio":
+					criterio.setMax_precio(Double.parseDouble(entry.getValue().toString()));
+					break;
+				case "fecha_anunciado":
+					
+					break;
+				case "fecha_ultima_actualizacion":
+					
+					break;
+			}
+		}
 		
 		try {
 			
-			listaAnuncio = claseAnuncio.obtenerTodos();
+			String busquedaSQL = criterio.criterioSQL();
+			listaAnuncio = claseAnuncio.listaInfoAnunciosCriterio(busquedaSQL);
 			
 		} catch (DAOException e) {
 			respuesta = Response.Status.INTERNAL_SERVER_ERROR;
@@ -92,7 +171,6 @@ public class ServicioAnuncio {
 			return Response.ok(listaAnuncio).build();
 		else
 			return Response.status(respuesta).build();
-		
 	}
 	
 	
@@ -106,7 +184,6 @@ public class ServicioAnuncio {
 		Response.Status respuesta = Response.Status.OK;
 		int filasModificadas = 0;
 		
-		
 		try {
 			
 			filasModificadas = claseAnuncio.modificar(id_inmueble, anuncio);
@@ -115,13 +192,10 @@ public class ServicioAnuncio {
 			respuesta = Response.Status.INTERNAL_SERVER_ERROR;
 		}
 		
-		
 		if(filasModificadas == 0) {
 			respuesta = Response.Status.NOT_FOUND;
 		}
-
 		return Response.status(respuesta).build();
-		
 	}
 	
 	
@@ -145,9 +219,7 @@ public class ServicioAnuncio {
 		if (filasModificadas == 0){
 			respuesta = Response.Status.NOT_FOUND;
 		}
-		
 		return Response.status(respuesta).build();
-		
 	}
 	
 
@@ -172,9 +244,7 @@ public class ServicioAnuncio {
 		if (filasModificadas == 0) {
 			respuesta = Response.Status.NOT_FOUND;
 		}
-		
 		return Response.status(respuesta).build();
-		
 	}
 	
 	
